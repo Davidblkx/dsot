299d0ac6dbf76b4c65de30fb2bc6434723c529b1
David Pires <developer@davidpires.pt>
2024-08-16 23:41:07 +0100

    1c534a9a-cea4-448f-ba6d-311f168eb391:Checkpoint saved at Fri, 16 Aug 2024 23:41:07 +0100 (now)

--- core/src/entities/mod.rs
+++ core/src/entities/mod.rs
@@ -7,3 +7,10 @@ pub mod tag_type;
 pub mod tag;
 pub mod album;
 pub mod release;
+
+pub use files::File;
+pub use artist::Artist;
+pub use tag_type::TagType;
+pub use tag::Tag;
+pub use album::Album;
+pub use release::Release;
