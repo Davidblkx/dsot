0413ae46a8caedfe61a3c0df173b921b239ed9ef
David Pires <developer@davidpires.pt>
2026-06-24 22:13:28 +0100

    658853ee-9b41-47ad-89f4-c3c445fdfb3e:Checkpoint saved at Wed, 24 Jun 2026 22:13:28 +0100 (now)

--- src/app/desktop/src/views/remote/mod.rs
+++ src/app/desktop/src/views/remote/mod.rs
@@ -1,13 +1,32 @@
+use std::str::FromStr;
+
 use dioxus::prelude::*;
 use dioxus_free_icons::{Icon, icons::ld_icons::LdRouter};
+use dsot_network::DsotNode;
+use dsot_shared_ui::widgets::network::MyAddress;
+use iroh::EndpointId;
 
 use crate::widgets::views::{Header, View};
-use dsot_shared_ui::widgets::network::MyAddress;
 
 static CSS: Asset = asset!("/assets/styles/view/remote.css");
 
 #[component]
 pub fn RemoteView() -> Element {
+    let mut address_str = use_signal(|| "".to_string());
+    let mut id: Signal<Option<EndpointId>> = use_signal(|| None);
+    let mut info: Signal<Option<DsotNode>> = use_signal(|| None);
+    let missing_address = use_memo(move || address_str.read().is_empty());
+    let missing_id = use_memo(move || !id.read().is_some());
+
+    let mut validate = move || {
+        let address_str = address_str.clone();
+        let txt = address_str.read();
+        match EndpointId::from_str(&txt) {
+            Ok(pub_id) => id.set(Some(pub_id)),
+            Err(_) => id.set(None),
+        }
+    };
+
     let icon = rsx! {
         Icon {
             icon: LdRouter
@@ -23,6 +42,27 @@ pub fn RemoteView() -> Element {
                 title: "Remote",
                 icon: icon,
                 MyAddress {  }
+            }
+
+            input {
+                value: address_str,
+                oninput: move |e| address_str.set(e.value()),
+            }
+
+            button {
+                disabled: missing_address,
+                onclick: move |_| {
+                    validate();
+                },
+                "Validate"
+            }
+
+            button {
+                disabled: missing_id,
+                onclick: move |_| {
+
+                },
+                "Connect"
             }
         }
     }
