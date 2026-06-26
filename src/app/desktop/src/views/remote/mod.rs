a6db89a1ecd87a67a240973f7b8ee911cc61bbd7
David Pires <developer@davidpires.pt>
2026-06-26 15:32:34 +0100

    Implement node connection logic in RemoteView
    Integrate DsotState to handle network connections and display node
    metadata
    upon successful discovery.

--- src/app/desktop/src/views/remote/mod.rs
+++ src/app/desktop/src/views/remote/mod.rs
@@ -2,6 +2,7 @@ use std::str::FromStr;
 
 use dioxus::prelude::*;
 use dioxus_free_icons::{Icon, icons::ld_icons::LdRouter};
+use dsot_lib::DsotState;
 use dsot_network::DsotNode;
 use dsot_shared_ui::widgets::network::MyAddress;
 use iroh::EndpointId;
@@ -12,9 +13,10 @@ static CSS: Asset = asset!("/assets/styles/view/remote.css");
 
 #[component]
 pub fn RemoteView() -> Element {
+    let state = use_context::<DsotState>();
     let mut address_str = use_signal(|| "".to_string());
     let mut id: Signal<Option<EndpointId>> = use_signal(|| None);
-    let mut info: Signal<Option<DsotNode>> = use_signal(|| None);
+    let info: Signal<Option<DsotNode>> = use_signal(|| None);
     let missing_address = use_memo(move || address_str.read().is_empty());
     let missing_id = use_memo(move || !id.read().is_some());
 
@@ -24,6 +26,22 @@ pub fn RemoteView() -> Element {
         match EndpointId::from_str(&txt) {
             Ok(pub_id) => id.set(Some(pub_id)),
             Err(_) => id.set(None),
+        }
+    };
+
+    let load_info = move || {
+        let net = state.clone().network.unwrap();
+        let mut info = info.clone();
+        if let Some(id) = *id.read() {
+            spawn(async move {
+                match net.connect_node(id).await {
+                    Ok(node) => info.set(Some(node)),
+                    Err(e) => {
+                        ::log::error!("Failed to connect node: {}", e);
+                        info.set(None)
+                    }
+                }
+            });
         }
     };
 
@@ -60,9 +78,25 @@ pub fn RemoteView() -> Element {
             button {
                 disabled: missing_id,
                 onclick: move |_| {
-
+                    load_info();
                 },
                 "Connect"
+            }
+
+            match &*info.read() {
+                None => rsx! {
+                    span { "Connect to a node first" }
+                },
+                Some(node) => rsx! {
+                    div {
+                        span { "Name: " }
+                        span { "{node.info.name}" }
+                    }
+                    div {
+                        span { "Desc: " }
+                        span { "{node.info.desc}" }
+                    }
+                }
             }
         }
     }
