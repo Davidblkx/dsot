fedebc21e29bcdba084dbbf62631d6de4bcd1153
David Pires <developer@davidpires.pt>
2026-08-11 23:06:04 +0100

    Refactor InboxItem to render icon and content label

--- src/app/desktop/src/views/inbox/item.rs
+++ src/app/desktop/src/views/inbox/item.rs
@@ -1,9 +1,22 @@
 use dioxus::prelude::*;
+use dioxus_free_icons::{Icon, icons::ld_icons::LdAirVent};
 use dsot_lib::state::inbox::InboxItemValue;
 
 #[component]
 pub fn InboxItem(item: InboxItemValue) -> Element {
+    let content = match item.value {
+        _ => rsx! {
+            Icon {
+                icon: LdAirVent
+            },
+            div {
+                class: "content",
+                "Unknown"
+            }
+        },
+    };
+
     rsx! {
-        "{item.id}"
+        {content}
     }
 }
